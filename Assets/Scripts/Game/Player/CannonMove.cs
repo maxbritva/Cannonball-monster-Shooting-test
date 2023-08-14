18f48190df9278cabca267481bf2c3eedc8d8ca7
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-08-14 16:45:32 +0300

    1

---
+++ Assets/Scripts/Game/Player/CannonMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game.Player
+{
+	public class CannonMove : MonoBehaviour
+	{
+		[SerializeField] private Transform _cannonTransform;
+		private Camera _camera;
+		private float _distance;
+
+		private void Awake() => _camera = Camera.main;
+		private void Update() => Move();
+
+		private void Move()
+		{
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+			Plane plane = new Plane(-Vector3.forward, Vector3.zero);
+			plane.Raycast(ray, out _distance);
+			Vector3 point = ray.GetPoint(_distance);
+			Vector3 toAim = point - transform.position;
+			//toAim.y = Mathf.Clamp(toAim.y, -10f,10f);
+			//transform.rotation = Quaternion.LookRotation(toAim);
+			transform.rotation = Quaternion.Euler(new Vector3(toAim.y,toAim.x));
+		}
+	}
+}
