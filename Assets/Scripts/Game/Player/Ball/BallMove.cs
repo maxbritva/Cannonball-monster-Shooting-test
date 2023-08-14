aed0680302d9f424bf560f5a14ca2a935d81161b
maxbritva <kirpich@list.ru>
2023-08-14 19:20:18 +0300

    Imported UI

---
+++ Assets/Scripts/Game/Player/Ball/BallMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Game.Player.Ball
+{
+public class BallMove : MonoBehaviour
+{
+  // [SerializeField] private RigidBody _rigidBody;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+}
+
